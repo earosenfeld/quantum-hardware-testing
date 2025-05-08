a7bd99f1984f7ac6fa554555d26df686dccac96a
Eric Rosenfeld <ericalanrosenfeld@gmail.com>
2025-05-07 22:46:07 -0700

    remove folder

--- cryocooler_test/src/utils.py
+++
@@ -1,8 +0,0 @@
-import random
-import time
-
-def simple_logger(msg):
-    print(f"[LOG] {time.strftime('%Y-%m-%d %H:%M:%S')} - {msg}")
-
-def inject_random_error(probability):
-    return random.random() < probability
--- cryocooler_test/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Test suite for the cryo_cooling_test package.
-"""
